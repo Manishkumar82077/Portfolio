35e0b5788a09a9ac60aac4678c818662b8331856
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2026-05-12 13:06:53 +0500

    Improve mobile responsiveness and fix layout spacing

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Heading({ FWord, LWord }) {
   return (
-    <h1 className="text-white text-center lg:text-left font-extrabold text-[54px] md:text-[94px] font-inter leading-tight m-0 p-0">
+    <h1 className="text-white text-center lg:text-left font-extrabold text-[54px] md:text-[80px] lg:text-[94px] font-inter leading-tight m-0 p-0">
       {FWord} <span style={{ color: "#2b2927" }}>{LWord}</span>
     </h1>
   );
